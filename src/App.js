d781435b6ad0c313f346f44a713f009710d0fc67
salt0s <josesansalt@gmail.com>
2021-07-21 20:25:13 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Comentario from './components/Comentario';
 import Saludo from './components/Saludo';
 
 function App() {
@@ -7,6 +8,25 @@ function App() {
 			<Saludo persona='Jose' edad={30} />
 			<Saludo persona='Juan' edad={15} />
 			<Saludo persona='Joseph' edad={24} />
+			<hr />
+			<h3>Cajita de comentarios</h3>
+			<Comentario
+				urlImagen='https://picsum.photos/64'
+				persona='Jose'
+				texto='aaaaaaaaaaaaaaaaaaa'
+			/>
+
+			<Comentario
+				urlImagen='https://picsum.photos/64'
+				persona='Juan'
+				texto='wwwwwwwwwwwwwwwwwwwwwww'
+			/>
+
+			<Comentario
+				urlImagen='https://picsum.photos/64'
+				persona='Joseph'
+				texto='dddddddddddddddddddd'
+			/>
 		</div>
 	);
 }
---
+++ src/components/Imagen.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Imagen = props => {
+	return <img src={props.urlImagen} className='me-3' alt='Img aleatoria' />;
+};
+
+export default Imagen;
